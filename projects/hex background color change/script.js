1fe7a042e5b358e573babf9a1b792ee1c1a7e670
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-04-12 21:35:35 +0530

    Converted all the lowercase alphabets in hexTable array to uppercase so that I needn't convert hexcolour to uppercase

--- projects/hex background color change/script.js
+++ projects/hex background color change/script.js
@@ -1,5 +1,5 @@
 `use strict`;
-const hexTable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+const hexTable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 // Math.round generates random nos between 0(inclusive) and 15(exclusive)
 // Math.random rounds number to nearest integer
 // eg. 2.45 to 2 , 2.54 to 3
@@ -9,7 +9,7 @@ setInterval(() => {
     const red = hexTable[Math.round(Math.random() * 15)] + hexTable[Math.round(Math.random() * 15)];
     const green = hexTable[Math.round(Math.random() * 15)] + hexTable[Math.round(Math.random() * 15)];
     const blue = hexTable[Math.round(Math.random() * 15)] + hexTable[Math.round(Math.random() * 15)];
-    const hexcolour = `#${red}${green}${blue}`.toUpperCase();
+    const hexcolour = `#${red}${green}${blue}`;
     hexSpan.innerText = hexcolour;
     document.body.style.backgroundColor = hexcolour;
 }, 1000);
