23c4d98962e2df7ebea544b4195c379ef06f4c02
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-04-12 21:25:25 +0530

    Generated random hex colour

---
+++ projects/hex background color change/script.js
@@ -0,0 +1,12 @@
+`use strict`;
+const hexTable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+// Math.round generates random nos between 0(inclusive) and 15(exclusive)
+// Math.random rounds number to nearest integer
+// eg. 2.45 to 2 , 2.54 to 3
+setInterval(() => {
+    const red = hexTable[Math.round(Math.random() * 15)] + hexTable[Math.round(Math.random() * 15)];
+    const green = hexTable[Math.round(Math.random() * 15)] + hexTable[Math.round(Math.random() * 15)];
+    const blue = hexTable[Math.round(Math.random() * 15)] + hexTable[Math.round(Math.random() * 15)];
+    const hexcolour = `#${red}${green}${blue}`;
+    console.log(hexcolour);
+}, 1000);
