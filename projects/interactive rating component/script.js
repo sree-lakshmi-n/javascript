102e2a6243471f46621eb11daf667bbfd6ec2c2f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-04-22 14:55:09 +0530

    Added event listeners to li elements and added clicked rating.

---
+++ projects/interactive rating component/script.js
@@ -0,0 +1,11 @@
+const ratingSection = document.getElementsByClassName("rating-container")[0];
+const thanksSection = document.getElementsByClassName("thanks-container")[0];
+const ul = document.getElementsByClassName("ratings")[0];
+const rating = document.getElementsByClassName("rating-obtained")[0];
+
+ul.addEventListener("click", function (e) {
+  // Check if the element is a LI
+  if (e.target.tagName === "LI") {
+    rating.innerText = e.target.innerText;
+  }
+});
