dd0fdf0ffea0cba46a51c46bdacde3524d48d340
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-04-22 14:55:37 +0530

    Hid the thanks section

--- projects/interactive rating component/script.js
+++ projects/interactive rating component/script.js
@@ -3,6 +3,9 @@ const thanksSection = document.getElementsByClassName("thanks-container")[0];
 const ul = document.getElementsByClassName("ratings")[0];
 const rating = document.getElementsByClassName("rating-obtained")[0];
 
+// Hiding the thanks section
+thanksSection.classList.add("hide");
+
 ul.addEventListener("click", function (e) {
   // Check if the element is a LI
   if (e.target.tagName === "LI") {
