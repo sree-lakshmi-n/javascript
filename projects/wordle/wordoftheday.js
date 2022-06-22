18efa83d6dfdf5c097c9fe57015130d8b295208b
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-22 18:29:55 +0530

    the date when game began

--- projects/wordle/wordoftheday.js
+++ projects/wordle/wordoftheday.js
@@ -1,1 +1,5 @@
 const { wordle_words, valid_words } = require(`${__dirname}/csvToArray.js`);
+
+// the date when game began
+// In original wordle, it is 19th of June 2021 (set by author)
+const gameBegin = new Date("29 Mar 2022").setHours(0, 0, 0, 0);
