61d63b207ad8ce3d245498d913fd2b00efdb6479
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-22 18:15:49 +0530

    Exporting Wordle words and Valid words array

--- projects/wordle/csvToArray.js
+++ projects/wordle/csvToArray.js
@@ -3,13 +3,14 @@ const fs = require("fs");
 
 //Converting csv to arrays
 const arr = (csv) => {
-  fs.readFileSync(csv)
+  return fs
+    .readFileSync(csv)
     .toString() // convert Buffer to string
     .split("\n") // split string to lines
     .map((e) => e.trim()) // remove white spaces for each line
     .map((e) => e.split(",").map((e) => e.trim())); // split each line to array
 };
 
-// Wordle words and Valid words array
-const wordle_words = arr("Wordle_Words.csv");
-const valid_words = arr("Valid_Words.csv");
+// Exporting Wordle words and Valid words array
+exports.wordle_words = arr("Wordle_Words.csv");
+exports.valid_words = arr("Valid_Words.csv");
