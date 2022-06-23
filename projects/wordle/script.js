5925ad8986228410a7fda2d28a42ec27359e79a9
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-23 16:53:34 +0530

    Shorthand function for getElementById function

--- projects/wordle/script.js
+++ projects/wordle/script.js
@@ -1,2 +1,11 @@
+// Word of the day
 const { wordoftheday } = require(`./wordoftheday`);
 console.log(wordoftheday);
+
+const gameArea = document.querySelector("#gamearea");
+const keyboardArea = document.querySelector("#keyboard");
+const messageDisplay = document.querySelector("#message");
+// Shorthand function for getElementById function
+const _ = (selector) => {
+  return document.getElementById(selector);
+};
