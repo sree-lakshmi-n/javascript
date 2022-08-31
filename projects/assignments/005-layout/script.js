0f0511d9a92da2ce94387b6d1e755d47ebf7d757
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-08-31 22:10:22 +0530

    Parent divs created

--- projects/assignments/005-layout/script.js
+++ projects/assignments/005-layout/script.js
@@ -1,1 +1,7 @@
 `use strict`;
+const collageNum = 37;
+for (let i = 0; i < collageNum; i++) {
+  const ele = document.createElement("div");
+  ele.className = `collage-${i + 1} collage`;
+  document.body.appendChild(ele);
+}
