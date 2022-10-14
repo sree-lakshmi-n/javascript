7ce2cb3587c776dc7bb78a51cf829eb90defb51b
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-10-14 10:28:37 +0530

    Memory game starter code

--- basics/array manipulation/script.js
+++ basics/array manipulation/script.js
@@ -1,1 +1,6 @@
 `use strict`;
+
+// 1. Write a JavaScript function to check whether an input is an array or not.
+const input = prompt("Enter input ");
+console.log(typeof input);
+console.log(Array.isArray(input));
