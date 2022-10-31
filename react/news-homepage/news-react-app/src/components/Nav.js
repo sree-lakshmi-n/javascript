4014b0e76478ceb8fdb725311c854c86cadc8b43
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-10-31 16:52:28 +0530

    Added Nav component html content and class names

---
+++ react/news-homepage/news-react-app/src/components/Nav.js
@@ -0,0 +1,16 @@
+function Nav() {
+  return (
+    <nav className="top-nav">
+      <div className="top-nav__logo"></div>
+      <ul className="top-nav__wrapper">
+        <li>Home</li>
+        <li>New</li>
+        <li>Popular</li>
+        <li>Popular</li>
+        <li>Categories</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
