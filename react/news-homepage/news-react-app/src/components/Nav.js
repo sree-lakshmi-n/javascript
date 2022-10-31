20ebdc630cb60886e8d2838b37978d2d73c35a29
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-10-31 17:20:46 +0530

    Added logo image to top nav

--- react/news-homepage/news-react-app/src/components/Nav.js
+++ react/news-homepage/news-react-app/src/components/Nav.js
@@ -1,7 +1,10 @@
+import logo from "../images/logo.svg";
 function Nav() {
   return (
     <nav className="top-nav">
-      <div className="top-nav__logo"></div>
+      <div className="top-nav__logo">
+        <img src={logo} alt="News Logo" className="logo" />
+      </div>
       <ul className="top-nav__wrapper">
         <li>Home</li>
         <li>New</li>
