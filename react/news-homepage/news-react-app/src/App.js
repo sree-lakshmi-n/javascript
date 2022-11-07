17850402f42a0a969bb6853efe30b6ce20d41dea
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-07 18:01:48 +0530

    added logo and nav items

--- react/news-homepage/news-react-app/src/App.js
+++ react/news-homepage/news-react-app/src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import Nav from "./components/UI/Nav";
+import Logo from "./components/Nav/Logo";
+import NavItems from "./components/Nav/NavItems";
 
 const App = () => {
-  return <Nav>Hello World!</Nav>;
+  return (
+    <Nav>
+      <Logo />
+      <NavItems />
+    </Nav>
+  );
 };
 
 export default App;
