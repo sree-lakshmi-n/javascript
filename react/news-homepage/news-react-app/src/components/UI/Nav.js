4fd0924754f742c40613fe6875a570429f562f32
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-07 18:03:08 +0530

    logic for nav classname

--- react/news-homepage/news-react-app/src/components/UI/Nav.js
+++ react/news-homepage/news-react-app/src/components/UI/Nav.js
@@ -1,8 +1,8 @@
 import "./Nav.css";
 
 const Nav = (props) => {
-  const classes = `nav ${props.className}`;
-  return <div className={classes}>{props.children}</div>;
+  const classes = `nav ${props.className ? props.className : ""}`;
+  return <nav className={classes}>{props.children}</nav>;
 };
 
 export default Nav;
