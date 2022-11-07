3dce5eed5a94a0559227e341f44138dc34e17401
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-07 18:01:29 +0530

    nav items added

---
+++ react/news-homepage/news-react-app/src/components/Nav/NavItems.js
@@ -0,0 +1,13 @@
+const NavItems = () => {
+  return (
+    <ul className="nav-items-wrapper">
+      <li className="nav-items">Home</li>
+      <li className="nav-items">New</li>
+      <li className="nav-items">Popular</li>
+      <li className="nav-items">Trending</li>
+      <li className="nav-items">Categories</li>
+    </ul>
+  );
+};
+
+export default NavItems;
