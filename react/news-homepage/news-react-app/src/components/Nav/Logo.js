593c86871cf2aded090c7374d8e947ad117ff89c
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-07 18:01:55 +0530

    logo added

---
+++ react/news-homepage/news-react-app/src/components/Nav/Logo.js
@@ -0,0 +1,10 @@
+import logo from "../../images/logo.svg";
+const Logo = () => {
+  return (
+    <div className="logo-wrapper">
+      <img src={logo} alt="company logo" className="logo" />
+    </div>
+  );
+};
+
+export default Logo;
