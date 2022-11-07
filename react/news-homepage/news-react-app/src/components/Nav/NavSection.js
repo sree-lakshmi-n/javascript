4a040f53e713498a32bdb189770629057c65587a
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-07 18:15:53 +0530

    nav section components imported and added

---
+++ react/news-homepage/news-react-app/src/components/Nav/NavSection.js
@@ -0,0 +1,13 @@
+import Nav from "../UI/Nav";
+import Logo from "./Logo";
+import NavItems from "./NavItems";
+
+const NavSection = () => {
+  return (
+    <Nav>
+      <Logo />
+      <NavItems />
+    </Nav>
+  );
+};
+export default NavSection;
