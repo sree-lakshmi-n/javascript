d067869f4ce804595bdad9e7e37e85a003ecbd64
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-07 17:19:22 +0530

    Added child elements

--- react/news-homepage/news-react-app/src/components/UI/Nav.js
+++ react/news-homepage/news-react-app/src/components/UI/Nav.js
@@ -2,7 +2,7 @@ import "./Nav.css";
 
 const Nav = (props) => {
   const classes = `nav ${props.className}`;
-  return <div className={classes}>props.children</div>;
+  return <div className={classes}>{props.children}</div>;
 };
 
 export default Nav;
