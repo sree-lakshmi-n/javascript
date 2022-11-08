c5826ce2a0865de8fd8c4c476ade4a30f8e137c9
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-08 22:08:30 +0530

    added content through props to top headline

--- react/news-homepage/news-react-app/src/components/News/TopHeadline.js
+++ react/news-homepage/news-react-app/src/components/News/TopHeadline.js
@@ -1,14 +1,16 @@
 import "./TopHeadline.css";
 
 const TopHeadline = (props) => {
+  const topHeadlineImg = require("../../images/image-web-3-desktop.jpg");
+
   return (
     <article className="top-headline">
       <div className="top-headline__img">
-        <img src={props.imageDesktop} />
+        <img src={topHeadlineImg} />
       </div>
       <div className="top-headline__textbox">
-        <h1 className="top-headline__title"></h1>
-        <p className="top-headline-content"></p>
+        <h1 className="top-headline__title">{props.headline.title}</h1>
+        <p className="top-headline-content">{props.headline.content}</p>
         <button className="read-more-btn">Read more</button>
       </div>
     </article>
