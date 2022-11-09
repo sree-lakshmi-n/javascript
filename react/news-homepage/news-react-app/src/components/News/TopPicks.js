0081148b88a2b29906c637281061bf0e204513c4
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-09 10:57:12 +0530

    imported top pick images and top pick items passing props

--- react/news-homepage/news-react-app/src/components/News/TopPicks.js
+++ react/news-homepage/news-react-app/src/components/News/TopPicks.js
@@ -1,32 +1,30 @@
 import "./TopPicks.css";
+import TopPicksItems from "./TopPicksItems";
+import img0 from "../../images/image-top-pick-1.jpg";
+import img1 from "../../images/image-top-pick-2.jpg";
+import img2 from "../../images/image-top-pick-3.jpg";
 
-const TopPicks = () => {
+const TopPicks = (props) => {
   return (
     <div className="top-picks">
-      <div className="top-picks__wrapper">
-        <div className="top-pick__img"></div>
-        <div className="top-pick__textbox">
-          <p className="top-pick__num">01</p>
-          <p className="top-pick__title">Reviving Retro PCs</p>
-          <p className="top-pick__content">
-            What happens when old PCs are given modern upgrades?
-          </p>
-        </div>
-      </div>
-      <div className="top-picks__wrapper">
-        <p className="top-pick__num">02</p>
-        <p className="top-pick__title">Top 10 Laptops of 2022</p>
-        <p className="top-pick__content">
-          Our best picks for various needs and budgets.
-        </p>
-      </div>
-      <div className="top-picks__wrapper">
-        <p className="top-pick__num">03</p>
-        <p className="top-pick__title">The Growth of Gaming</p>
-        <p className="top-pick__content">
-          How the pandemic has sparked fresh opportunities.
-        </p>
-      </div>
+      <TopPicksItems
+        image={img0}
+        num={props.topItem.num[0]}
+        title={props.topItem.title[0]}
+        content={props.topItem.content[0]}
+      />
+      <TopPicksItems
+        image={img1}
+        num={props.topItem.num[1]}
+        title={props.topItem.title[1]}
+        content={props.topItem.content[1]}
+      />
+      <TopPicksItems
+        image={img2}
+        num={props.topItem.num[2]}
+        title={props.topItem.title[2]}
+        content={props.topItem.content[2]}
+      />
     </div>
   );
 };
