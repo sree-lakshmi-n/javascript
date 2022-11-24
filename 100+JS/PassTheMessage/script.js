350853479170a63f61ea5524dd1e1700e3ee8d92
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-24 15:19:29 +0530

    Adding the last message in last message container on clicking submit

--- 100+JS/PassTheMessage/script.js
+++ 100+JS/PassTheMessage/script.js
@@ -1,1 +1,18 @@
 `use strict`;
+const userInput = document.getElementsByClassName("message")[0];
+const submitBtn = document.getElementsByClassName("submit")[0];
+const lastMsgContainer = document.getElementsByClassName(
+  "last-message-container"
+)[0];
+
+lastMsgContainer.style.display = "none";
+
+// Adding the last message in last message container on clicking submit
+submitBtn.addEventListener("click", () => {
+  lastMsgContainer.style.display = "block";
+  const lastMsg = document.createElement("p");
+  lastMsg.setAttribute("class", "last-message");
+  lastMsg.textContent = userInput.value;
+  userInput.value = ""; // resetting input value
+  lastMsgContainer.appendChild(lastMsg);
+});
