a07811c134df26ae2b6dd442fc1d14fd875c18bd
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-24 14:47:11 +0530

    deleted newapp folder

--- react/news-homepage/news-react-app/src/components/UI/Nav.js
+++
@@ -1,8 +0,0 @@
-import "./Nav.css";
-
-const Nav = (props) => {
-  const classes = `nav ${props.className ? props.className : ""}`;
-  return <nav className={classes}>{props.children}</nav>;
-};
-
-export default Nav;
