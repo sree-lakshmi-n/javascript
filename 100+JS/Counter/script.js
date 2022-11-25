98525143b194d1be94764d90e7bcb2692e2cb6e7
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-25 11:19:48 +0530

    lower count, add count, and counter color change functionalities

--- 100+JS/Counter/script.js
+++ 100+JS/Counter/script.js
@@ -1,1 +1,22 @@
 `use strict`;
+const root = document.querySelector(":root");
+const counter = document.getElementsByClassName("counter-display")[0];
+const lowerCount = document.getElementsByClassName("lower-count")[0];
+const addCount = document.getElementsByClassName("add-count")[0];
+
+lowerCount.addEventListener("click", () => {
+  counter.textContent = +counter.textContent - 1;
+  changeCounterColor();
+});
+addCount.addEventListener("click", () => {
+  counter.textContent = +counter.textContent + 1;
+  changeCounterColor();
+});
+const changeCounterColor = () => {
+  if (+counter.textContent >= 0) {
+    // getting css variables in js
+    counter.style.color = getComputedStyle(root).getPropertyValue("--green");
+  } else {
+    counter.style.color = getComputedStyle(root).getPropertyValue("--red");
+  }
+};
