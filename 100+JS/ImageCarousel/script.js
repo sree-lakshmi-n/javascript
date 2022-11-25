95df9a790ae05155c796ed0a38c26f69eb6abc60
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-25 12:28:05 +0530

    prev and next btn functionality

--- 100+JS/ImageCarousel/script.js
+++ 100+JS/ImageCarousel/script.js
@@ -1,1 +1,20 @@
 `use strict`;
+const prevBtn = document.getElementsByClassName("btn-prev")[0];
+const nextBtn = document.getElementsByClassName("btn-next")[0];
+const image = document.getElementsByClassName("image")[0];
+
+let count = 1;
+const addCount = () => {
+  count = count + 1 > 4 ? 1 : count + 1;
+  return count;
+};
+const reduceCount = () => {
+  count = count - 1 < 1 ? 4 : count - 1;
+  return count;
+};
+prevBtn.addEventListener("click", () => {
+  image.src = `./images/image-${reduceCount()}.jpeg`;
+});
+nextBtn.addEventListener("click", () => {
+  image.src = `./images/image-${addCount()}.jpeg`;
+});
